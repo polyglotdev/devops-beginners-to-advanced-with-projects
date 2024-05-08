ff726f97b4ab98b815b5bc5f66b426fbce8deb7c
Dom Hallan <dom@domhallan.com>
2024-05-08 18:31:48 -0400

    chore: Add function to calculate sum of a list of numbers

--- section-6/app.py
+++ section-6/app.py
@@ -4,3 +4,16 @@ def add(x, y):
 
 result = add(1, 2)
 print(result)
+
+# list
+my_list = [1, 2, 3]
+
+
+def print_list(list):
+    sum = 0
+    for item in list:
+        sum += item
+    return sum
+
+
+print(print_list(my_list))
